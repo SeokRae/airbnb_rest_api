b46d2c614d56a9764e2bed6d1c87dcf941cb1e96
seokrae <kslbsh@gmail.com>
2020-04-22 19:22:29 +0900

    2020.04.22 json to Django object Serializers

---
+++ rooms/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+# Data Serializers
+class RoomSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    bedrooms = serializers.IntegerField()
+    instant_book = serializers.BooleanField()
